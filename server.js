e22f35dff5e8d63e9b259c2761b4038d5593b03d
Zogbelemou Jean Charles <jeancharles.zogbelemou@student.arteveldehs.be>
2025-05-15 13:43:08 +0200

    Afbeelding uploaden multer installatie

--- server.js
+++ server.js
@@ -33,7 +33,41 @@ app.get('/', (req, res) => {
   });
 });
 
+app.post('/menu', (req, res) => {
+  const { name, category, price, available_date } = req.body;
+  db.run(
+    `INSERT INTO producten (naam, categorie, prijs, datum) VALUES (?, ?, ?, ?)`,
+    [name, category, price, available_date],
+    function (err) {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).send('Fout bij opslaan in database');
+      }
+      console.log(`Nieuw product toegevoegd met ID ${this.lastID}`);
+      res.redirect('/');
+    }
+  );
+});
+
+app.post('/menu/edit', (req, res) => {
+  const { id, name, category, price, available_date } = req.body;
+  db.run(
+    `UPDATE producten SET naam = ?, categorie = ?, prijs = ?, datum = ? WHERE id = ?`,
+    [name, category, price, available_date, id],
+    function (err) {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).send('Fout bij bewerken in database');
+      }
+      console.log(`Product met ID ${id} bewerkt.`);
+      res.redirect('/');
+    }
+  );
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server draait op http://localhost:${port}`);
 });
+
+
