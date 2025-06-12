6867c717194c1cff08b98f05529a6bb407df01a3
Zogbelemou Jean Charles <jeancharles.zogbelemou@student.arteveldehs.be>
2025-06-12 16:44:53 +0200

    Tentative de correction SQLite: ouverture en lecture seule

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const multer = require('multer');
 const expressLayouts = require('express-ejs-layouts');
-const sqlite3 = require('sqlite3').verbose();
+
 //const port = 3009;
+
+const sqlite3 = require('sqlite3').verbose();
+// Ouvre la base de données en mode lecture seule
+const db = new sqlite3.Database('./fastfood.db', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        // En cas d'erreur, affichez un message pour le débogage dans les logs Vercel
+        console.error('Erreur lors de l\'ouverture de la base de données en mode lecture seule:', err.message);
+        // Vous pouvez décider de faire un process.exit(1) ici pour arrêter l'appli si la DB ne s'ouvre pas du tout
+        // Ou simplement laisser l'appli essayer de continuer si elle n'a pas besoin de la DB immédiatement
+    } else {
+        console.log('Base de données fastfood.db ouverte avec succès en mode lecture seule.');
+    }
+});
+
 
 const port = process.env.PORT || 3009;
 
